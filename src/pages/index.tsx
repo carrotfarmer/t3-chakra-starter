ee5a96fc7880bde9872b288ba427104740fcf064
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-02-16 14:01:04 +0530

    feat: navbar chakra ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Flex, Stack, IconButton, Text, Button, useColorModeValue, Heading } from "@chakra-ui/react";
 import { type NextPage } from "next";
 import Head from "next/head";
 // import Link from "next/link";
@@ -16,7 +17,56 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        hello
+        <Box>
+          <Flex
+            bg={useColorModeValue('white', 'gray.800')}
+            color={useColorModeValue('gray.600', 'white')}
+            minH={'60px'}
+            py={{ base: 2 }}
+            px={{ base: 4 }}
+            borderBottom={1}
+            borderStyle={'solid'}
+            borderColor={useColorModeValue('gray.200', 'gray.900')}
+            align={'center'}
+          >
+            <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+              <Heading color={useColorModeValue('gray.800', 'white')} size="md">
+                Logo
+              </Heading>
+
+            </Flex>
+            <Stack
+              flex={{ base: 1, md: 0 }}
+              justify={'flex-end'}
+              direction={'row'}
+              spacing={6}
+            >
+              <Button
+                as={'a'}
+                fontSize={'sm'}
+                fontWeight={400}
+                variant={'link'}
+                href={'#'}>
+                Sign In
+              </Button>
+              <Button
+                as={'a'}
+                display={{ base: 'none', md: 'inline-flex' }}
+                fontSize={'sm'}
+                fontWeight={600}
+                color={'white'}
+                bg={'pink.400'}
+                href={'#'}
+                _hover={{
+                  bg: 'pink.300',
+                }}>
+                Sign Up
+              </Button>
+            </Stack>
+          </Flex>
+
+        </Box>
+
       </main>
     </>
   )
