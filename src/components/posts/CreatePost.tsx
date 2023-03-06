f58684a74c358799f1afc9b48c8dc98da4b41851
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-03-06 22:58:47 +0530

    feat: form validation w/ zod

--- src/components/posts/CreatePost.tsx
+++ src/components/posts/CreatePost.tsx
@@ -1,41 +1,51 @@
 import React from "react";
 
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
 import { FormErrorMessage, FormLabel, FormControl, Input, Button, Box } from "@chakra-ui/react";
+
+import { createPostConstants } from "../../lib/constants";
 
 interface CreatePostProps {}
 
 export const CreatePost: React.FC<CreatePostProps> = ({}) => {
+  const { MIN_LENGTH, MAX_LENGTH } = createPostConstants;
+
+  const createPostSchema = z.object({
+    name: z
+      .string()
+      .min(MIN_LENGTH, { message: "min length is 2 chars" })
+      .max(MAX_LENGTH, {
+        message: `the maximum length is ${MAX_LENGTH} chars`,
+      }),
+  });
+
   const {
     handleSubmit,
     register,
-    formState: { errors, isSubmitting },
-  } = useForm();
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(createPostSchema),
+  });
 
   return (
     <Box>
       <form onSubmit={handleSubmit((data) => console.log(data))}>
         <FormControl isInvalid={Boolean(errors.name)}>
           <FormLabel htmlFor="name">post name</FormLabel>
-          <Input
-            id="name"
-            placeholder="my first post"
-            {...register("name", {
-              required: "a name is required",
-              minLength: { value: 2, message: "min length is 2 chars" },
-            })}
-          />
+          <Input id="name" placeholder="my first post" {...register("name")} />
           <FormErrorMessage>{errors.name && errors.name.message?.toString()}</FormErrorMessage>
         </FormControl>
         <Box pt="5">
           <Button
-            as={"a"}
             display={{ base: "none", md: "inline-flex" }}
             fontSize={"sm"}
             fontWeight={600}
             color={"white"}
             bg={"pink.400"}
-            href={"#"}
+            type="submit"
             _hover={{
               bg: "pink.300",
             }}
---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const createPostConstants = {
+  MAX_LENGTH: 50,
+  MIN_LENGTH: 2,
+}
+
