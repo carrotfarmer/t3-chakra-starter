f39b2c8c5cd3914de864ddd35efd7410c88c8de3
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-03-08 20:36:26 +0530

    feat: delete post mutation

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -3,25 +3,39 @@ import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const postRouter = createTRPCRouter({
   createPost: protectedProcedure
-  .input(
-    z.object({ 
-      name: z.string()
-    }))
-  .mutation(async ({ input, ctx }) => {
-    return await ctx.prisma.post.create({
-      data: {
-        name: input.name,
-        userId: ctx.session.user.id
-      }
+    .input(
+      z.object({
+        name: z.string(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      return await ctx.prisma.post.create({
+        data: {
+          name: input.name,
+          userId: ctx.session.user.id,
+        },
+      });
+    }),
+
+  getPosts: protectedProcedure.query(async ({ ctx }) => {
+    return await ctx.prisma.post.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
     });
   }),
 
-  getPosts: protectedProcedure
-  .query(async ({ ctx }) => {
-    return await ctx.prisma.post.findMany({
-      where: {
-        userId: ctx.session.user.id
-      }
-    })
-  })
+  deletePost: protectedProcedure
+    .input(
+      z.object({
+        postId: z.string(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      return await ctx.prisma.post.delete({
+        where: {
+          id: input.postId,
+        },
+      });
+    }),
 });
